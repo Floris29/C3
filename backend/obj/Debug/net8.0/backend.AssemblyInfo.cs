a813a21fbf07c3941d185baf7c948edda67a3947
Floris29 <Florismelchers@gmail.com>
2024-10-30 10:13:51 +0100

    npm run serve command

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0307347b3bf05c97b8d211710df14d42e8b82f46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82341737762b730ae086a2efe1d1252883846eba")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
